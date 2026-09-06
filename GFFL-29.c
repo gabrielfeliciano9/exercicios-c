f05412aab0d42033534eb4d5e5b3d2873bb0fcad
Gabriel Feliciano <gfelicianofreitas@gmail.com>
2026-09-06 13:55:50 -0300

    Update GFFL-29.c

--- GFFL-29.c
+++ GFFL-29.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-	printf("\n* Aluno: Gabriel Feliciano de Freitas Leal - RA 0026908       *");
+	
 	printf("\n* Programa GFFL-29 -  Numeros pares de 0 a 50                 *");
 	printf("\n***************************************************************");
 	

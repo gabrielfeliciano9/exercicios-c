9246371b003b0ed41871058f10e64a22c0abfd38
Gabriel Feliciano <gfelicianofreitas@gmail.com>
2026-09-06 13:48:55 -0300

    Update GFFL-19.c

--- GFFL-19.c
+++ GFFL-19.c
@@ -3,7 +3,6 @@
 
 int main(){
 	
-	printf("\n* Aluno: Gabriel Feliciano de Freitas Leal - RA 0026908     *");
 	printf("\n* Programa GFFL-19 -   Ordem crescente (tres numeros)       *");
 	printf("\n*************************************************************");
 	
